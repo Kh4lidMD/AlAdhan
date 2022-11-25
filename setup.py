0741e9c4df0c9a5581859208858964a1027b8278
Khaled <72672455+Kh4lidMD@users.noreply.github.com>
2022-11-25 09:37:45 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aladhan",
+    version="1.0",
+    author="Khaled Mahmoud",
+    author_email="KhalidYBel@gmail.com",
+    description="A Python package to calculate Islamic prayer times for any location in the world.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/Kh4lidMD/adhan_api",
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 4 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Natural Language :: Arabic',
+        'Natural Language :: English',
+        'Software Development :: Libraries'
+        'License :: OSI Approved :: MIT License'
+    ]
+)
