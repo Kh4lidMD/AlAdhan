075b11419e6e68c151fa0eb3670414bd47b19505
Khaled <72672455+Kh4lidMD@users.noreply.github.com>
2022-11-29 16:32:29 +0300

    changed version argument to latest (3.0.0 alpha)

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="aladhan",
-    version="2.2.0",
+    name="aladhan-api",
+    version="3.0.0",
     author="Khaled Mahmoud",
     author_email="KhalidYBel@gmail.com",
     description="A Python package to calculate Islamic prayer times for any location in the world.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.github.com/Kh4lidMD/adhan_api",
+    url="https://www.github.com/Kh4lidMD/AlAdhan",
     packages=find_packages(),
     install_requires=['requests'],
     classifiers=[
-        'Development Status :: 4 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Natural Language :: Arabic',
         'Natural Language :: English',
-        'Software Development :: Libraries'
+        'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License'
     ]
 )
