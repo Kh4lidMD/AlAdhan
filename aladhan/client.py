3e7bbffa2502c9e9c720b668c980f5dcd0dee65f
Khaled <72672455+Kh4lidMD@users.noreply.github.com>
2022-12-02 03:22:24 +0300

    replaced get_prayer_times with 3 new methods

--- aladhan/client.py
+++ aladhan/client.py
@@ -7,6 +7,7 @@ import requests
 
 
 API = 'https://api.aladhan.com/v1/'
+SKIP = ['Sunrise', 'Sunset', 'Midnight', 'Lastthird', 'Firstthird', 'Imsak']
 
 
 class Aladhan:
@@ -21,14 +22,38 @@ class Aladhan:
         """
         self.default_location = default_location
 
-    def get_prayer_times(
+
+    @staticmethod
+    def __set_location_params(params: dict, location: Union[Coordinates, Address, City]):
+        if isinstance(location, Coordinates):
+            params['latitude'] = location.latitude
+            params['longitude'] = location.longitude
+        elif isinstance(location, Address):
+            params['address'] = location.url_encoded()
+        elif isinstance(location, City):
+            params['city'] = location.city
+            params['country'] = location.country
+            if location.state:
+                params['state'] = location.state
+
+
+    @staticmethod
+    def __get_endpoint_by_location(location: Union[Coordinates, Address, City], endpoint: str):
+        if isinstance(location, Coordinates):
+            return endpoint
+        if isinstance(location, Address):
+            return endpoint + 'ByAddress'
+        if isinstance(location, City):
+            return endpoint + 'ByCity'
+        
+    
+    def get_calendar_times(
         self, 
         location: Union[Coordinates, Address, City] = None, 
-        year: int=None, month: int=None,
-        today_only=False
+        year: int=None, month: int=None
     ) -> list[Adhan]:
         """
-        Get the prayer times for a specific month (current month by default) and returns a list of `adhan_api.Adhan` objects.
+        Get the prayer times for a specific month (current month by default) and returns a list of `aladhan.adhan.Adhan` objects.
 
         - `location: Union[Coordinates, Address, City]=None` is the location to get the prayer times for.
         - `year: int=None` is the year of the prayer times.
@@ -47,55 +72,113 @@ class Aladhan:
             else:
                 location = self.default_location
 
-        # get the location type and the ENDPOINT
-        if isinstance(location, Coordinates):
-            ENDPOINT = 'calendar'
-            params['latitude'] = location.latitude
-            params['longitude'] = location.longitude
-        elif isinstance(location, Address):
-            ENDPOINT = 'calendarByAddress'
-            params['address'] = location.address
-        elif isinstance(location, City):
-            ENDPOINT = 'calendarByCity'
-            params['city'] = location.city
-            params['country'] = location.country
-            if location.state:
-                params['state'] = location.state
-        else:
-            raise TypeError('Invalid location type.')
-        
-        # today only
-        if today_only:
-            ENDPOINT = ENDPOINT.replace('calendar', 'timings')
+        # set year and month
+        if year:
+            params['year'] = year
+        if month:
+            params['month'] = month
+
+        # set the location type and the ENDPOINT
+        self.__set_location_params(params, location)
+        ENDPOINT = self.__get_endpoint_by_location(location, 'calendar')
         
         # send the request and get the JSON data
         response = requests.get(API + ENDPOINT, params=params)
         data = response.json()
 
         # loop through the data and create a list of Adhan objects
-        
-        # for entire month
-        if not today_only:
-            adhan_list = []
-            for day in data['data']:
-                date = datetime.strptime(day['date']['gregorian']['date'], '%d-%m-%Y')
-                for salah_name, salah_time in day['timings'].items():
-                    if salah_name in ['Sunrise', 'Sunset', 'Midnight', 'Lastthird', 'Firstthird', 'Imsak']:
-                        continue
-                    salah_time = datetime.strptime(salah_time.split(" ")[0], '%H:%M')
-                    salah_time = salah_time.replace(year=date.year, month=date.month, day=date.day)
-                    adhan_list.append(Adhan(salah_name, salah_time))
-            return adhan_list
-        
-        # for today only
-        else:
-            adhan_list = []
-            date = datetime.now()
-            for salah_name, salah_time in data['data']['timings'].items():
-                if salah_name in ['Sunrise', 'Sunset', 'Midnight', 'Lastthird', 'Firstthird', 'Imsak']:
+        adhan_list = []
+        for day in data['data']:
+            date = datetime.strptime(day['date']['gregorian']['date'], '%d-%m-%Y')
+            for salah_name, salah_time in day['timings'].items():
+                if salah_name in SKIP:
                     continue
                 salah_time = datetime.strptime(salah_time.split(" ")[0], '%H:%M')
                 salah_time = salah_time.replace(year=date.year, month=date.month, day=date.day)
                 adhan_list.append(Adhan(salah_name, salah_time))
-            return adhan_list
+        return adhan_list
 
+
+    def get_today_times(self, location: Union[Coordinates, Address, City] = None) -> list[Adhan]:
+        """
+        Get the prayer times for today and returns a list of 5 `aladhan.adhan.Adhan` objects.
+
+        - `location: Union[Coordinates, Address, City]=None` is the location to get the prayer times for.
+
+        Note: If `location` is not provided, the default location that was provided when initializing the class will be used.
+        """
+
+        # define parameters
+        params = {}
+
+        # figure out the location if it was in the argument or in the default location
+        if location is None:
+            if self.default_location is None:
+                raise ValueError('No location was provided.')
+            else:
+                location = self.default_location
+
+        # set the location type and the ENDPOINT
+        self.__set_location_params(params, location)
+        ENDPOINT = self.__get_endpoint_by_location(location, 'timings')
+        
+        # send the request and get the JSON data
+        response = requests.get(API + ENDPOINT, params=params)
+        data = response.json()
+
+        # loop through the data and create a list of Adhan objects
+        adhan_list = []
+        for salah_name, salah_time in data['data']['timings'].items():
+            if salah_name in SKIP:
+                continue
+            salah_time = datetime.strptime(salah_time.split(" ")[0], '%H:%M')
+            salah_time = salah_time.replace(year=datetime.now().year, month=datetime.now().month, day=datetime.now().day)
+            adhan_list.append(Adhan(salah_name, salah_time))
+        return adhan_list
+
+
+    def get_annual_times(self, location: Union[Coordinates, Address, City] = None, year: int=None) -> list[Adhan]:
+        """
+        Get the prayer times for a specific year (current year by default) and returns a list of `aladhan.adhan.Adhan` objects.
+        
+        - `location: Union[Coordinates, Address, City]=None` is the location to get the prayer times for.
+        - `year: int=None` is the year of the prayer times (current year by default).
+        
+        Note: If `location` is not provided, the default location that was provided when initializing the class will be used.
+        """
+        
+        # define parameters
+        params = {'annual': 'true'}
+        
+        # figure out the location if it was in the argument or in the default location
+        if location is None:
+            if self.default_location is None:
+                raise ValueError('No location was provided.')
+            else:
+                location = self.default_location
+        
+        # set year
+        if year:
+            params['year'] = year
+            
+        # set the location type and the ENDPOINT
+        self.__set_location_params(params, location)
+        ENDPOINT = self.__get_endpoint_by_location(location, 'calendar')
+        
+        # send the request and get the JSON data
+        response = requests.get(API + ENDPOINT, params=params)
+        data = response.json()
+        
+        # loop through the data and create a list of Adhan objects
+        adhan_list = []
+        for month in data['data']:
+            for day in data['data'][month]:
+                date = datetime.strptime(day['date']['gregorian']['date'], '%d-%m-%Y')
+                for salah_name, salah_time in day['timings'].items():
+                    if salah_name in SKIP:
+                        continue
+                    salah_time = datetime.strptime(salah_time.split(" ")[0], '%H:%M')
+                    salah_time = salah_time.replace(year=date.year, month=date.month, day=date.day)
+                    adhan_list.append(Adhan(salah_name, salah_time))
+        
+        return adhan_list
