9db732f2d2c7eebc6f005e613a3fb5e10d07b7b0
Khaled <KhalidYBel@gmail.com>
2022-12-15 21:51:35 +0300

    added docstrings for exceptions

--- aladhan/exceptions.py
+++ aladhan/exceptions.py
@@ -1,3 +1,19 @@
-class RateLimitedException(Exception):pass
-class BadRequestException(Exception):pass
-class ServerErrorException(Exception):pass
+class RateLimitedException(Exception):
+    """
+    Exception raised when the API rate limit is exceeded, according to the
+    API:
+    
+    `For the AlAdhan API in each region, each IP is allowed approximately 14 requests per second.`
+    """
+    pass
+
+class BadRequestException(Exception):
+    """Exception raised when a bad request is made to the API (e.g. invalid date)."""
+    pass
+
+class ServerErrorException(Exception):
+    """
+    This exception is raised when the API returns a 500 error (internal server error), 
+    which is commonly happening in the Aladhan API for non-handled errors.
+    """
+    pass
